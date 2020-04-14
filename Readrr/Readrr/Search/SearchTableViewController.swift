568fc4bc0cddcccc32fc1c3d9547f31eb48b023c
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-04-14 16:58:42 -0700

    basic search function working with mock data

--- Readrr/Readrr/Search/SearchTableViewController.swift
+++ Readrr/Readrr/Search/SearchTableViewController.swift
@@ -9,75 +9,40 @@
 import UIKit
 
 class SearchTableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        // 3
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(updateTableView),
+                                               name: .updateEmbeddedTVC,
+                                               object: nil)
+    }
+    
+    // Called when notification is heard
+    @objc func updateTableView() {
+        print("updatingTVC")
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1 // Change later?
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return myBooksArray.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "resultCell", for: indexPath)
-
-        // Configure the cell...
+        
+        cell.textLabel?.text = myBooksArray[indexPath.row]
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -85,6 +50,9 @@ class SearchTableViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
+
+let fakeBooksArray = ["Harry Potter", "Twilight", "Animal Farm", "1984", "Metamorphosis", "50 Shades of Gray",
+                        "Resident Evil", "Jumanji", "The Bible", "The Maze Runner", "Fahrenheit 451"]
+var myBooksArray = [String]()
--- Readrr/Readrr/Search/SearchViewController.swift
+++ Readrr/Readrr/Search/SearchViewController.swift
@@ -8,16 +8,44 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController  {
 
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     
+    static var searchTerm = "" // global
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchBar?.delegate = self
+        searchBar?.placeholder = "Search for book by title or author"
     }
-
 
 }
 
+extension SearchViewController: UISearchBarDelegate {
+
+    // Search when
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+
+        guard let searchTerm = searchBar.text else {
+            print("Empty searchbar")
+            return
+        }
+        print(searchTerm)
+        // searchFuntion() then DispatchQueue.main.async {self.tableView.reloadData()}
+        
+        let booksWithSearchTerm = fakeBooksArray.filter {
+            $0.lowercased().contains(searchTerm.lowercased())
+        }
+        myBooksArray = booksWithSearchTerm
+        
+        // 2
+        NotificationCenter.default.post(name: .updateEmbeddedTVC, object: self)
+    }
+}
+
+// 1
+extension NSNotification.Name {
+    static let updateEmbeddedTVC = NSNotification.Name("updateEmbeddedTVC")
+}
