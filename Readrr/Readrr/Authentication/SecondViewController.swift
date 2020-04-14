354402abe7e3ad3e1e96cfd976ae3fbcad90c0f3
Alexander Supe <alex.m.supe@gmail.com>
2020-04-14 17:39:09 +0200

    Initial Setup

---
+++ Readrr/Readrr/Authentication/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Readrr
+//
+//  Created by Alexander Supe on 4/14/20.
+//  Copyright © 2020 Alexander Supe. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
