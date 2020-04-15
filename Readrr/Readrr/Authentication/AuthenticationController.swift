2a1d24512f336472e70537f4585559541e8f744a
Alexander Supe <alex.m.supe@gmail.com>
2020-04-15 23:46:33 +0200

    Extracted Keychain logic

--- Readrr/Readrr/Authentication/AuthenticationController.swift
+++ Readrr/Readrr/Authentication/AuthenticationController.swift
@@ -21,20 +21,24 @@ class AuthenticationController {
         
         AF.request(url, method: .post, parameters: user, encoder: JSONParameterEncoder.default).validate().responseJSON { (response) in
             completion(response.result)
-            switch response.result {
-            case .success(let data):
-                KeychainSwift.shared.set(email, forKey: "email")
-                KeychainSwift.shared.set(password, forKey: "password")
-                if let name = name {
-                    KeychainSwift.shared.set(name, forKey: "name")
-                } else if let dict = data as? [String: Any],
-                    let user = dict["user"] as? [String: Any],
-                    let name = user["fullName"] as? String {
-                    KeychainSwift.shared.set(name, forKey: "name")
-                }
-            case .failure:
-                break
+            saveToKeychain(result: response.result, email: email, password: password, name: name)
+        }
+    }
+    
+    static private func saveToKeychain(result: AFResult<Any>, email: String, password: String, name: String?) {
+        switch result {
+        case .success(let data):
+            KeychainSwift.shared.set(email, forKey: "email")
+            KeychainSwift.shared.set(password, forKey: "password")
+            if let name = name {
+                KeychainSwift.shared.set(name, forKey: "name")
+            } else if let dict = data as? [String: Any],
+                let user = dict["user"] as? [String: Any],
+                let name = user["fullName"] as? String {
+                KeychainSwift.shared.set(name, forKey: "name")
             }
+        case .failure:
+            break
         }
     }
 }
