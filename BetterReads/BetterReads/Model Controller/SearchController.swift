81792fe71cdb4aa21b7b223ba41e4eeae80e2c9f
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-04-23 15:55:27 -0700

    added SearchController file

---
+++ BetterReads/BetterReads/Model Controller/SearchController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchController.swift
+//  BetterReads
+//
+//  Created by Jorge Alvarez on 4/23/20.
+//  Copyright © 2020 Labs23. All rights reserved.
+//
+
+import Foundation
+
+class SearchController {
+    
+    let baseUrl = URL(string: "https://pokeapi.co/api/v2/pokemon/gengar")!
+}
