ead5e1e4b3ee198adc4f16d7eb158c952c7a4836
CC <ciarabeitel@gmail.com>
2020-05-04 16:05:19 -0400

    Add RecommendationCollectionViewCell.swift file, add image view to cell for book cover.

---
+++ BetterReads/BetterReads/Views/RecommendationCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  RecommendationCollectionViewCell.swift
+//  BetterReads
+//
+//  Created by Ciara Beitel on 5/4/20.
+//  Copyright © 2020 Labs23. All rights reserved.
+//
+
+import UIKit
+
+class RecommendationCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var bookCoverImageView: UIImageView!
+    
+    
+}
