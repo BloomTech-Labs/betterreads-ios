997433e1f3fde51cfd16a29c9c391b770377b06b
CC <ciarabeitel@gmail.com>
2020-05-04 15:40:03 -0400

    Add logo and account settings image placeholders, add HomeViewController.swift file.

---
+++ BetterReads/BetterReads/View Controllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  BetterReads
+//
+//  Created by Ciara Beitel on 5/4/20.
+//  Copyright © 2020 Labs23. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
