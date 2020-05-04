40b96bc4b71856dac37816ae8bfd21bfe3a10716
CC <ciarabeitel@gmail.com>
2020-05-04 15:59:13 -0400

    Hook up outlets on home screen.

--- BetterReads/BetterReads/View Controllers/HomeViewController.swift
+++ BetterReads/BetterReads/View Controllers/HomeViewController.swift
@@ -9,22 +9,23 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    //MARK: - Properties
+    
+    //MARK: - Outlets
+    @IBOutlet weak var welcomeUser: UILabel!
+    @IBOutlet weak var topRecommendationLabel: UILabel!
+    @IBOutlet weak var middleRecommendationLabel: UILabel!
+    @IBOutlet weak var bottomRecommendationLabel: UILabel!
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
         navigationController?.navigationBar.isHidden = true
     }
     
+    //MARK: - Methods
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
