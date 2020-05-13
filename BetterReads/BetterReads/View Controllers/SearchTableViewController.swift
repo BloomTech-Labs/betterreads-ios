ab5e37f7fbc0d42df80f3d6a91005e9107fe982a
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-05-13 14:38:27 -0700

    adds segue identifiers to all segues going to detailVC, including searchTVC.

--- BetterReads/BetterReads/View Controllers/SearchTableViewController.swift
+++ BetterReads/BetterReads/View Controllers/SearchTableViewController.swift
@@ -81,6 +81,9 @@ class SearchTableViewController: UITableViewController {
     // FIXME: add segue that goes to BookDetailViewController?
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SearchToDetail" {
+            print("SearchToDetail")
+        }
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
