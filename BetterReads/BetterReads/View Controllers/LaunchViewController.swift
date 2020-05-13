2b7fdfd350fedcbfc0bfeb2ef7be3ba08544de11
CC <ciarabeitel@gmail.com>
2020-05-13 18:13:22 -0400

    Add images to assests and create a launch screen view controller on the main.storyboard.
    Add animations to launch screen view controller.
    
    Refactor code, delete blank lines.
    
    Change function name.
    
    Decrease delay on fade out.
    
    Edit size of circleBook image on main SB.

---
+++ BetterReads/BetterReads/View Controllers/LaunchViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LaunchViewController.swift
+//  BetterReads
+//
+//  Created by Ciara Beitel on 5/13/20.
+//  Copyright © 2020 Labs23. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+    
+    @IBOutlet weak var blobBackgroundImage: UIImageView!
+    @IBOutlet weak var circleBookImage: UIImageView!
+    @IBOutlet weak var dashedCircleImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.5, delay: 0.3, options: .curveEaseIn, animations: {
+            self.blobBackgroundImage.transform = CGAffineTransform(scaleX: 2.5, y: 2.5)
+            self.dashedCircleImage.transform = CGAffineTransform(rotationAngle: -20.0)
+        })
+        UIView.animate(withDuration: 1.5, delay: 0.3, options: .curveEaseIn, animations: {
+            self.blobBackgroundImage.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            self.dashedCircleImage.transform = CGAffineTransform(rotationAngle: -20.0)
+        })
+        UIView.animate(withDuration: 2.0, delay: 1.0, options: .curveEaseIn, animations: {
+            self.blobBackgroundImage.alpha = 0.0;
+            self.circleBookImage.alpha = 0.0
+            self.dashedCircleImage.alpha = 0.0
+        })
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.5, execute: {
+            self.performSegue(withIdentifier: "ShowMainScreen", sender: nil)
+        })
+    }
+}
