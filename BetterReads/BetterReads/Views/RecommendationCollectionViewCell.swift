c5131006e939f5a2d27bfee6dbb7484353a20e9d
CC <ciarabeitel@gmail.com>
2020-05-19 21:07:22 -0400

    Change straight quotes to curly quotes.

--- BetterReads/BetterReads/Views/RecommendationCollectionViewCell.swift
+++ BetterReads/BetterReads/Views/RecommendationCollectionViewCell.swift
@@ -20,33 +20,29 @@ class RecommendationCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var bookCoverImageView: UIImageView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     private func getBookThumbnail() {
-        defaultBookImageTitle.isHidden = true
-        defaultBookImageAuthor.isHidden = true
         bookCoverImageView.layer.cornerRadius = 5
         guard let book = book else {
             activityIndicator.stopAnimating()
-            defaultBookImageTitle.isHidden = true
-            defaultBookImageAuthor.isHidden = true
             return
         }
-        if let smallThumbnailUrl = URL(string: book.smallThumbnail ?? "") {
+        if let smallThumbnail = book.smallThumbnail, let smallThumbnailUrl = URL(string: smallThumbnail) {
             let options = ImageLoadingOptions(
                 placeholder: UIImage(named: "BetterReads-DefaultBookImage"),
                 transition: .fadeIn(duration: 0.33)
             )
             Nuke.loadImage(with: smallThumbnailUrl, options: options, into: bookCoverImageView)
-        }
-        updateViews()
-    }
-    private func updateViews() {
-        if bookCoverImageView.image == UIImage(named: "BetterReads-DefaultBookImage") {
+            return updateViews()
+        } else {
+            defaultBookImageTitle.text = """
+                                        \u{201C}\(book.title ?? "Title not available")\u{201D}
+                                        """
+            defaultBookImageAuthor.text = "by \(book.authors?.first ?? "Unknown")"
             defaultBookImageTitle.isHidden = false
             defaultBookImageAuthor.isHidden = false
-            defaultBookImageTitle.text = book?.title
-            defaultBookImageAuthor.text = book?.authors?.first
+            return updateViews()
         }
-        defaultBookImageTitle.isHidden = true
-        defaultBookImageAuthor.isHidden = true
+    }
+    private func updateViews() {
         activityIndicator.stopAnimating()
         self.setNeedsLayout()
     }
