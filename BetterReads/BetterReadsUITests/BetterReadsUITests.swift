ffc667fc1c53d1a892e29fdc1ded322debeffdeb
CC <ciarabeitel@gmail.com>
2020-05-20 19:00:48 -0400

    Add UI tests.

--- BetterReads/BetterReadsUITests/BetterReadsUITests.swift
+++ BetterReads/BetterReadsUITests/BetterReadsUITests.swift
@@ -9,37 +9,49 @@
 import XCTest
 
 class BetterReadsUITests: XCTestCase {
-    
     private var app: XCUIApplication {
         return XCUIApplication()
     }
-
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-        
-        // UI tests must launch the application that they test.
         app.launch()
     }
-
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        
+    func testSignInScreenShowing() {
+        let signInSegBtn = app.firstMatch.scrollViews.segmentedControls.buttons["Sign in"]
+        let firstLabel = app.staticTexts["Email"]
+        if signInSegBtn.isSelected {
+            XCTAssertTrue(firstLabel.exists)
+        }
     }
-
+    func testSignUpScreenShowing() {
+        let signUpSegBtn = app.firstMatch.scrollViews.segmentedControls.buttons["Sign up"]
+        signUpSegBtn.tap()
+        let firstLabel = app.staticTexts["Full name"]
+        if signUpSegBtn.isSelected {
+            XCTAssertTrue(firstLabel.exists)
+        }
+    }
+//    func testPasswordsMatch() {
+//        let signUpSegBtn = app.firstMatch.scrollViews.segmentedControls.buttons["Sign up"]
+//        signUpSegBtn.tap()
+//        let passwordTF = app.secureTextFields.firstMatch
+//        let confirmTF = app.secureTextFields.element(boundBy: 1)
+//        passwordTF.tap()
+//        passwordTF.typeText("abcde123")
+//        passwordTF.typeText("\n")
+//        confirmTF.tap()
+//        confirmTF.typeText("abcde123")
+//        confirmTF.typeText("\n")
+//    }
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
             measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
                 XCUIApplication().launch()
             }
+            // Tested on May 20, 2020 at 6:21pm EDT
+            // Average launch = 1.003 secs
         }
     }
 }
