5caa1d76c0b394c43ff09be9f3bd6cde30c0c373
CC <ciarabeitel@gmail.com>
2020-05-20 17:36:11 -0400

    Add more star ratings tests.

--- BetterReads/BetterReadsTests/BetterReadsTests.swift
+++ BetterReads/BetterReadsTests/BetterReadsTests.swift
@@ -60,8 +60,8 @@ class BetterReadsTests: XCTestCase {
         XCTAssertEqual(searchResultView.starsArray[3].image, emptyStarImage)
         XCTAssertEqual(searchResultView.starsArray[4].image, emptyStarImage)
     }
-    func testHalfStarRating() {
-        let halfStarBook = Book(authors: ["Ms. Writer"],
+    func testOneHalfStarRating() {
+        let oneHalfStarBook = Book(authors: ["Ms. Writer"],
                                 categories: ["Fiction"],
                                 itemDescription: "Description",
                                 googleID: "123",
@@ -78,11 +78,236 @@ class BetterReadsTests: XCTestCase {
                                 isbn10: "", isbn13: "",
                                 publishedDate: "")
         let searchResultView = SearchResultView()
-        searchResultView.book = halfStarBook
+        searchResultView.book = oneHalfStarBook
         XCTAssertEqual(searchResultView.starsArray[0].image, halfFilledStarImage)
         XCTAssertEqual(searchResultView.starsArray[1].image, emptyStarImage)
         XCTAssertEqual(searchResultView.starsArray[2].image, emptyStarImage)
         XCTAssertEqual(searchResultView.starsArray[3].image, emptyStarImage)
         XCTAssertEqual(searchResultView.starsArray[4].image, emptyStarImage)
+    }
+    func testOneFullStarRating() {
+        let oneFullStarBook = Book(authors: ["Ms. Writer"],
+                                categories: ["Fiction"],
+                                itemDescription: "Description",
+                                googleID: "123",
+                                isEbook: false,
+                                language: "English",
+                                pageCount: 33,
+                                publisher: "A press",
+                                smallThumbnail: "",
+                                textSnippet: "Text snippet",
+                                thumbnail: "",
+                                title: "Test Title",
+                                webReaderLink: "",
+                                averageRating: 1.0,
+                                isbn10: "", isbn13: "",
+                                publishedDate: "")
+        let searchResultView = SearchResultView()
+        searchResultView.book = oneFullStarBook
+        XCTAssertEqual(searchResultView.starsArray[0].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[1].image, emptyStarImage)
+        XCTAssertEqual(searchResultView.starsArray[2].image, emptyStarImage)
+        XCTAssertEqual(searchResultView.starsArray[3].image, emptyStarImage)
+        XCTAssertEqual(searchResultView.starsArray[4].image, emptyStarImage)
+    }
+    func testOneAndHalfFullStarRating() {
+        let oneAndHalfFullStarBook = Book(authors: ["Ms. Writer"],
+                                categories: ["Fiction"],
+                                itemDescription: "Description",
+                                googleID: "123",
+                                isEbook: false,
+                                language: "English",
+                                pageCount: 33,
+                                publisher: "A press",
+                                smallThumbnail: "",
+                                textSnippet: "Text snippet",
+                                thumbnail: "",
+                                title: "Test Title",
+                                webReaderLink: "",
+                                averageRating: 1.6,
+                                isbn10: "", isbn13: "",
+                                publishedDate: "")
+        let searchResultView = SearchResultView()
+        searchResultView.book = oneAndHalfFullStarBook
+        XCTAssertEqual(searchResultView.starsArray[0].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[1].image, halfFilledStarImage)
+        XCTAssertEqual(searchResultView.starsArray[2].image, emptyStarImage)
+        XCTAssertEqual(searchResultView.starsArray[3].image, emptyStarImage)
+        XCTAssertEqual(searchResultView.starsArray[4].image, emptyStarImage)
+    }
+    func testTwoFullStarRating() {
+        let twoFullStarBook = Book(authors: ["Ms. Writer"],
+                                categories: ["Fiction"],
+                                itemDescription: "Description",
+                                googleID: "123",
+                                isEbook: false,
+                                language: "English",
+                                pageCount: 33,
+                                publisher: "A press",
+                                smallThumbnail: "",
+                                textSnippet: "Text snippet",
+                                thumbnail: "",
+                                title: "Test Title",
+                                webReaderLink: "",
+                                averageRating: 2.1,
+                                isbn10: "", isbn13: "",
+                                publishedDate: "")
+        let searchResultView = SearchResultView()
+        searchResultView.book = twoFullStarBook
+        XCTAssertEqual(searchResultView.starsArray[0].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[1].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[2].image, emptyStarImage)
+        XCTAssertEqual(searchResultView.starsArray[3].image, emptyStarImage)
+        XCTAssertEqual(searchResultView.starsArray[4].image, emptyStarImage)
+    }
+    func testTwoAndHalfFullStarRating() {
+        let twoAndHalfFullStarBook = Book(authors: ["Ms. Writer"],
+                                categories: ["Fiction"],
+                                itemDescription: "Description",
+                                googleID: "123",
+                                isEbook: false,
+                                language: "English",
+                                pageCount: 33,
+                                publisher: "A press",
+                                smallThumbnail: "",
+                                textSnippet: "Text snippet",
+                                thumbnail: "",
+                                title: "Test Title",
+                                webReaderLink: "",
+                                averageRating: 2.44,
+                                isbn10: "", isbn13: "",
+                                publishedDate: "")
+        let searchResultView = SearchResultView()
+        searchResultView.book = twoAndHalfFullStarBook
+        XCTAssertEqual(searchResultView.starsArray[0].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[1].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[2].image, halfFilledStarImage)
+        XCTAssertEqual(searchResultView.starsArray[3].image, emptyStarImage)
+        XCTAssertEqual(searchResultView.starsArray[4].image, emptyStarImage)
+    }
+    func testThreeFullStarRating() {
+        let threeFullStarsBook = Book(authors: ["Ms. Writer"],
+                                categories: ["Fiction"],
+                                itemDescription: "Description",
+                                googleID: "123",
+                                isEbook: false,
+                                language: "English",
+                                pageCount: 33,
+                                publisher: "A press",
+                                smallThumbnail: "",
+                                textSnippet: "Text snippet",
+                                thumbnail: "",
+                                title: "Test Title",
+                                webReaderLink: "",
+                                averageRating: 3.0,
+                                isbn10: "", isbn13: "",
+                                publishedDate: "")
+        let searchResultView = SearchResultView()
+        searchResultView.book = threeFullStarsBook
+        XCTAssertEqual(searchResultView.starsArray[0].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[1].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[2].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[3].image, emptyStarImage)
+        XCTAssertEqual(searchResultView.starsArray[4].image, emptyStarImage)
+    }
+    func testThreeAndHalfFullStarRating() {
+        let threeAndHalfFullStarBook = Book(authors: ["Ms. Writer"],
+                                categories: ["Fiction"],
+                                itemDescription: "Description",
+                                googleID: "123",
+                                isEbook: false,
+                                language: "English",
+                                pageCount: 33,
+                                publisher: "A press",
+                                smallThumbnail: "",
+                                textSnippet: "Text snippet",
+                                thumbnail: "",
+                                title: "Test Title",
+                                webReaderLink: "",
+                                averageRating: 3.55,
+                                isbn10: "", isbn13: "",
+                                publishedDate: "")
+        let searchResultView = SearchResultView()
+        searchResultView.book = threeAndHalfFullStarBook
+        XCTAssertEqual(searchResultView.starsArray[0].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[1].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[2].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[3].image, halfFilledStarImage)
+        XCTAssertEqual(searchResultView.starsArray[4].image, emptyStarImage)
+    }
+    func testFourFullStarRating() {
+        let fourFullStarsBook = Book(authors: ["Ms. Writer"],
+                                categories: ["Fiction"],
+                                itemDescription: "Description",
+                                googleID: "123",
+                                isEbook: false,
+                                language: "English",
+                                pageCount: 33,
+                                publisher: "A press",
+                                smallThumbnail: "",
+                                textSnippet: "Text snippet",
+                                thumbnail: "",
+                                title: "Test Title",
+                                webReaderLink: "",
+                                averageRating: 3.99,
+                                isbn10: "", isbn13: "",
+                                publishedDate: "")
+        let searchResultView = SearchResultView()
+        searchResultView.book = fourFullStarsBook
+        XCTAssertEqual(searchResultView.starsArray[0].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[1].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[2].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[3].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[4].image, emptyStarImage)
+    }
+    func testFourAndHalfFullStarRating() {
+        let fourAndHalfFullStarBook = Book(authors: ["Ms. Writer"],
+                                categories: ["Fiction"],
+                                itemDescription: "Description",
+                                googleID: "123",
+                                isEbook: false,
+                                language: "English",
+                                pageCount: 33,
+                                publisher: "A press",
+                                smallThumbnail: "",
+                                textSnippet: "Text snippet",
+                                thumbnail: "",
+                                title: "Test Title",
+                                webReaderLink: "",
+                                averageRating: 4.65,
+                                isbn10: "", isbn13: "",
+                                publishedDate: "")
+        let searchResultView = SearchResultView()
+        searchResultView.book = fourAndHalfFullStarBook
+        XCTAssertEqual(searchResultView.starsArray[0].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[1].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[2].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[3].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[4].image, halfFilledStarImage)
+    }
+    func testFiveFullStarRating() {
+        let fiveFullStarsBook = Book(authors: ["Ms. Writer"],
+                                categories: ["Fiction"],
+                                itemDescription: "Description",
+                                googleID: "123",
+                                isEbook: false,
+                                language: "English",
+                                pageCount: 33,
+                                publisher: "A press",
+                                smallThumbnail: "",
+                                textSnippet: "Text snippet",
+                                thumbnail: "",
+                                title: "Test Title",
+                                webReaderLink: "",
+                                averageRating: 5.0,
+                                isbn10: "", isbn13: "",
+                                publishedDate: "")
+        let searchResultView = SearchResultView()
+        searchResultView.book = fiveFullStarsBook
+        XCTAssertEqual(searchResultView.starsArray[0].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[1].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[2].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[3].image, fullStarImage)
+        XCTAssertEqual(searchResultView.starsArray[4].image, fullStarImage)
     }
 }
