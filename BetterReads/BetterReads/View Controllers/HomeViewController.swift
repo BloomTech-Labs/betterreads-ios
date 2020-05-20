775d5fef84588cc9ba6cd0b970ddb73929a41b21
CC <ciarabeitel@gmail.com>
2020-05-20 11:25:09 -0400

    Add line breaks.

--- BetterReads/BetterReads/View Controllers/HomeViewController.swift
+++ BetterReads/BetterReads/View Controllers/HomeViewController.swift
@@ -28,6 +28,7 @@ Flip through the tailored recommendations below from a variety of authors and st
     @IBOutlet weak var middleCollectionView: UICollectionView!
     @IBOutlet weak var bottomRecommendationLabel: UILabel!
     @IBOutlet weak var bottomCollectionView: UICollectionView!
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +39,7 @@ Flip through the tailored recommendations below from a variety of authors and st
         middleCollectionView.dataSource = self
         bottomCollectionView.delegate = self
         bottomCollectionView.dataSource = self
+        
         if (UserController.shared.isNewUser ?? false) {
             welcomeMessage.text = welcomeNewUserMessageText
         } else {
