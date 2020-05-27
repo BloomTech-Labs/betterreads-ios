30035f5dbad882cf3f1d90b1b7a23864beef9fd0
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-05-27 11:11:01 -0700

    adds webview to view web reader links

--- BetterReads/BetterReads/Resources/Helpers.swift
+++ BetterReads/BetterReads/Resources/Helpers.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+extension URL {
+    var usingHTTPS: URL? {
+        guard var components = URLComponents(url: self, resolvingAgainstBaseURL: true) else { return nil }
+        components.scheme = "https"
+        return components.url
+    }
+}
+
 // Animation
 extension UIView {
     func performFlare() {
